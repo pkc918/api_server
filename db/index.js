36073b3792ad11be64dcaa8d3962e5c3d500ed92
XiaoPan <2489964425@qq.com>
2022-09-06 09:48:46 +0800

    createConnection 更换 createPool 使用

--- db/index.js
+++ db/index.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: "127.0.0.1:9000",
+const db = mysql.createPool({
+    host: "127.0.0.1",
     user: "root",
     password: "~123qwer",
     database: "my_db_01"
 });
+
 
 module.exports = db;
