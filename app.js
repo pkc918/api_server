da6e9c26a1a8db35df9a205d5a2f01a8b46fb65e
XiaoPan <2489964425@qq.com>
2022-09-06 17:55:16 +0800

    封装中间件捕获 joi 的错误

--- app.js
+++ app.js
@@ -4,27 +4,31 @@ const app = express();
 const cors = require("cors"); // cors 中间件
 
 // 中间件
-const beforeSend = function (req, res, next) {
+app.use(cors()); // 注册为全局可用中间件
+
+// 只能解析 application/x-www-form-urlencoded 格式的表数据
+app.use(express.urlencoded({extended: false}));
+
+// 做返回前的数据处理
+app.use((req, res, next) => {
     /* status
     *   1：错误
     *   0：成功
     * */
-    res.beforeSend = function (message, status = 1) {
-        return res.send({
+    res.beforeSend = (message, status = 1) => {
+        res.send({
             status,
             message: message instanceof Error ? message.message : message
         });
     };
     next();
-};
-app.use(beforeSend); // 做返回前的数据处理
-
-app.use(cors()); // 注册为全局可用中间件
-
-// 只能解析 application/x-www-form-urlencoded 格式的表数据
-app.use(express.urlencoded({extended: false}));
+});
 //
 app.use("/api", userRouter);
+
+app.use((err, req, res, next) => {
+    res.beforeSend(err, 1);
+});
 
 app.listen(9000, () => {
     console.log("blogApi running at http://127.0.0.1:9000");
