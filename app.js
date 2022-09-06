e6158477ff6c5d5144d873fcce0edfd128067735
XiaoPan <2489964425@qq.com>
2022-09-06 16:44:52 +0800

    封装 res.beforeSend 函数

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const userRouter = require("./router/user"); // 导入用户路由模块
 const express = require("express");
 const app = express();
-const cors = require("cors") // cors 中间件
+const cors = require("cors"); // cors 中间件
 
 // 中间件
-app.use(cors()) // 注册为全局可用中间件
+const beforeSend = function (req, res, next) {
+    /* status
+    *   1：错误
+    *   0：成功
+    * */
+    res.beforeSend = function (message, status = 1) {
+        return res.send({
+            status,
+            message: message instanceof Error ? message.message : message
+        });
+    };
+    next();
+};
+app.use(beforeSend); // 做返回前的数据处理
+
+app.use(cors()); // 注册为全局可用中间件
+
 // 只能解析 application/x-www-form-urlencoded 格式的表数据
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 //
-app.use('/api', userRouter)
+app.use("/api", userRouter);
 
 app.listen(9000, () => {
     console.log("blogApi running at http://127.0.0.1:9000");
-})
+});
--- router/user_handlers.js
+++ router/user_handlers.js
@@ -1,35 +1,70 @@
 const db = require("../db/index");
+const bcrypt = require("bcryptjs");
 exports.registerUser = (req, res) => {
     const checkUserName = /^[\w\u4e00-\u9fa5]{1,10}$/;
-    const checkPassword = /^\w{6,18}$/;
-    const {username, password} = req.body;
+    const checkPassword = /^[\S]{6,18}$/;
+    let {username, password} = req.body;
     // 当用户名或者密码为空
     if (!username || !password) {
-        return res.send({
-            status: 1, message: "用户名或者密码不能为空！"
-        });
+        return res.beforeSend(`用户名或密码不能为空`, 1);
+        // return res.send({
+        //     status: 1, message: "用户名或者密码不能为空！"
+        // });
     }
     // 判断用户名和密码满足要求
     if (!checkUserName.test(username.trim()) || !checkPassword.test(password.trim())) {
-        return res.send({
-            status: 1, message: "用户名(1-10位)或者密码(6-18位)不满足条件！"
-        });
+        return res.beforeSend(`用户名(1-10位)或者密码(6-18位)不满足条件！`, 1);
+        // return res.send({
+        //     status: 1, message: "用户名(1-10位)或者密码(6-18位)不满足条件！"
+        // });
     }
     // 判断该是否已经创建
-    db.query(`select * from ev_users where username=?`, [username], ((err, result) => {
-        if (err) {
-            return res.send({
-                message: err.message,
-                status: 1
-            });
-        }
-        if (result.length > 0) {
-            return res.send({
-                message: "用户已被占用!",
-                status: 1
-            });
-        }
-    }));
+    db.query(
+      `select * from ev_users where username=?`,
+      [username],
+      (err, result) => {
+          if (err) {
+              return res.beforeSend(err, 1);
+              // return res.send({
+              //     message: err.message,
+              //     status: 1
+              // });
+          }
+          if (result.length > 0) {
+              return res.beforeSend(`用户名已被使用`, 1);
+              // return res.send({
+              //     message: "用户已被占用!",
+              //     status: 1
+              // });
+          }
+          // 可用的用户名和密码
+          password = bcrypt.hashSync(password, 10);
+          // 插入用户表
+          db.query(
+            `insert into ev_users(username, password) values(?,?)`,
+            [username, password],
+            (err, result) => {
+                if (err) {
+                    return res.beforeSend(err, 1);
+                    // return res.send({
+                    //     status: 1, message: err.message
+                    // });
+                }
+                // 影响行数为 1 行
+                if (result.affectedRows !== 1) {
+                    return res.beforeSend(`sql error`, 1);
+                    // return res.send({
+                    //     status: 1, message: ""
+                    // });
+                }
+                return res.beforeSend(`注册成功`, 0);
+                // res.send({
+                //     status: 0,
+                //     message: "注册成功!"
+                // });
+            }
+          );
+      });
 };
 
 exports.login = (req, res) => {
