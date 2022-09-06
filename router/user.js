c1e85951af135edd62f12248204ff16bab89e68c
XiaoPan <2489964425@qq.com>
2022-09-06 17:56:16 +0800

    使用封装函数 schema_handler 和 验证规则schema 替换if语句

--- router/user.js
+++ router/user.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const userHandlers = require("./user_handlers");
-const router = express.Router();
+const {schema_Handlers} = require("../schema/schema_Handlers");
+const {schema} = require("../schema/user");
 
+const router = express.Router();
 // 注册新用户
-router.post("/register", userHandlers.registerUser);
+router.post("/register", schema_Handlers(schema), userHandlers.registerUser);
 // 用户登录
-router.post("/login",userHandlers.login);
+router.post("/login", userHandlers.login);
 
 // 导出路由
 module.exports = router;
