63e17aafa382c0d8355a14a9cd54681f4ae59f74
XiaoPan <2489964425@qq.com>
2022-09-06 21:26:48 +0800

    新增 token认证 错误中间件

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const userRouter = require("./router/user"); // 导入用户路由模块
 const express = require("express");
+const {expressjwt: JWT} = require("express-jwt");
+const {jwtSecretKey} = require("./config");
 const app = express();
-const cors = require("cors"); // cors 中间件
+const cors = require("cors");// cors 中间件
+const joi = require("joi");
 
 // 中间件
 app.use(cors()); // 注册为全局可用中间件
 
 // 只能解析 application/x-www-form-urlencoded 格式的表数据
 app.use(express.urlencoded({extended: false}));
+
+// 指令哪些接口不需要 token 身份认证
+app.use(JWT({secret: jwtSecretKey, algorithms: ["HS256"]}).unless({path: [/^\/api\//]}));
 
 // 做返回前的数据处理
 app.use((req, res, next) => {
@@ -23,11 +29,19 @@ app.use((req, res, next) => {
     };
     next();
 });
-//
 app.use("/api", userRouter);
 
+// 捕获错误的中间件
 app.use((err, req, res, next) => {
-    return res.beforeSend(err, 1);
+    if (err instanceof joi.ValidationError) return res?.send({status: 1, message: err?.message});
+    if (err?.name === "UnauthorizedError") {
+        return res?.send({
+            status: 1,
+            message: "invalid token..."
+        });
+    } else {
+        next(err);
+    }
 });
 
 app.listen(9000, () => {
