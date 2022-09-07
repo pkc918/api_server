2363a253e42064ff15f93400b09fe480ca412319
XiaoPan <2489964425@qq.com>
2022-09-07 21:53:47 +0800

    实现 updatePassword 函数

--- router/userinfo_handlers.js
+++ router/userinfo_handlers.js
@@ -1,4 +1,5 @@
 const db = require("../db/index");
+const {compareSync, hashSync} = require("bcryptjs");
 
 // 获取用户信息
 exports.getUserInfo = (req, res) => {
@@ -29,7 +30,32 @@ exports.updateUserInfo = (req, res) => {
       (err, result) => {
           if (err) return res.beforeSend(err, 502);
           if (result.affectedRows !== 1) return res.beforeSend(`修改未成功!`, 502);
-          return res.beforeSend(`修改用户信息成功!`, 200)
+          return res.beforeSend(`修改用户信息成功!`, 200);
+      }
+    );
+};
+
+// 修改密码
+exports.updatePassword = (req, res) => {
+    db.query(
+      `select * from ev_users where id = ?`,
+      [req.body.id],
+      (err, result) => {
+          if (err) return res.beforeSend(err, 501);
+          if (result.length !== 1) return res.beforeSend(`修改密码失败`, 502);
+          // 判断 旧密码（明文）是否与数据库中密码(加密过得)相等
+          if (!compareSync(req.body.oldPassword, result[0].password)) {
+              return res.beforeSend(`原密码错误`, 400);
+          }
+          db.query(
+            `update ev_users set password = ? where id = ?`,
+            [hashSync(req.body.newPassword, 10), req.body.id],
+            (err, result) => {
+                if (err) return res.beforeSend(err, 502);
+                if (result.affectedRows !== 1) return res.beforeSend(`修改密码失败`, 502);
+                return res.beforeSend(`修改密码成功!`, 200);
+            }
+          );
       }
     );
 };
