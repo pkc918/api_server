99b259251601394b776e3d8b90fa3d636fc835e2
XiaoPan <2489964425@qq.com>
2022-09-07 11:19:55 +0800

    修改规则名称 schema -> login_schema

--- router/user.js
+++ router/user.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const userHandlers = require("./user_handlers");
 const {schema_Handlers} = require("../schema/schema_Handlers");
-const {schema} = require("../schema/user");
+const {login_schema} = require("../schema/user");
 
 const router = express.Router();
 // 注册新用户
-router.post("/register", schema_Handlers(schema), userHandlers.registerUser);
+router.post("/register", schema_Handlers(login_schema), userHandlers.registerUser);
 // 用户登录
-router.post("/login", schema_Handlers(schema),userHandlers.login);
+router.post("/login", schema_Handlers(login_schema),userHandlers.login);
 
 // 导出路由
 module.exports = router;
