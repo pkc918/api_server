e8e6e19e01b0d409467df306f9cc07cd37737ba5
XiaoPan <2489964425@qq.com>
2022-09-07 11:20:47 +0800

    更新用户信息接口模块

--- router/userinfo.js
+++ router/userinfo.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const {getUserInfo} = require("./userinfo_handlers");
+const {schema_Handlers} = require("../schema/schema_Handlers");
+const {getUserInfo, updateUserInfo} = require("./userinfo_handlers");
+const {userInfo_schema} = require("../schema/user");
+
 
 const router = express.Router();
 // 获取用户基本信息
 router.get("/userinfo", getUserInfo);
+// 更新用户基本信息
+router.post("/userinfo", schema_Handlers(userInfo_schema), updateUserInfo);
 
 // 导出路由
 module.exports = router;
--- router/userinfo_handlers.js
+++ router/userinfo_handlers.js
@@ -1,5 +1,6 @@
 const db = require("../db/index");
 
+// 获取用户信息
 exports.getUserInfo = (req, res) => {
     db.query(
       `select id, username, nickname, password, user_pic, email from ev_users where id = ?`,
@@ -15,7 +16,20 @@ exports.getUserInfo = (req, res) => {
               message: `获取用户信息成功!`,
               data: result[0],
               status: 200
-          })
+          });
+      }
+    );
+};
+
+// 更新用户信息
+exports.updateUserInfo = (req, res) => {
+    db.query(
+      `update ev_users set ? where id = ?`,
+      [req.body, req.body.id],
+      (err, result) => {
+          if (err) return res.beforeSend(err, 502);
+          if (result.affectedRows !== 1) return res.beforeSend(`修改未成功!`, 502);
+          return res.beforeSend(`修改用户信息成功!`, 200)
       }
     );
 };
