99e50b3ee3d2ef7a976cd220456c84dff2337278
XiaoPan <2489964425@qq.com>
2022-09-10 12:00:24 +0800

    补充：alias 参数规则

--- schema/articles.js
+++ schema/articles.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 
 exports.add_ArticleCate = {
     body: {
+        alias: Joi.string().alphanum().required(),
         title: Joi.string().pattern(/^[\S\u4e00-\u9fa5]{1,4}$/).required()
     }
 };
