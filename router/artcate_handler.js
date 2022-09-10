032b2c7e5de1f08910e1c33f8feb879fb3b4bfd2
XiaoPan <2489964425@qq.com>
2022-09-10 12:00:43 +0800

    实现 addArticleCate 函数功能

--- router/artcate_handler.js
+++ router/artcate_handler.js
@@ -13,3 +13,28 @@ exports.getArticleCates = (req, res) => {
       }
     );
 };
+
+exports.addArticleCate = (req, res) => {
+    const values = [req.body.title, req.body.alias];
+    db.query(
+      `select name, alias from ev_article_cate where name = ? || alias = ?`,
+      values,
+      (err, result) => {
+          if (err) return res.beforeSend(err, 502);
+          if (result.length > 0) return res.beforeSend(`标签已存在!`, 502);
+          db.query(
+            `insert into ev_article_cate(name, alias) values(?, ?)`,
+            values,
+            (err, result) => {
+                if (err) return res.beforeSend(err, 502);
+                if (result.affectedRows !== 1) return res.beforeSend(`添加标签失败!`, 502);
+                return res.send({
+                    status: 201,
+                    message: `创建成功!`
+                });
+            }
+          );
+      }
+    );
+
+};
