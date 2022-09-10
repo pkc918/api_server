a9e31102cf490a7f463a4af0f1363e91bc55bb56
XiaoPan <2489964425@qq.com>
2022-09-10 11:31:15 +0800

    验证参数合法性

--- router/artcate.js
+++ router/artcate.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {getArticleCates} = require("./artcate_handler");
+const {schema_Handlers} = require("../schema/schema_Handlers");
+const {add_ArticleCate} = require("../schema/articles");
+const {getArticleCates, addArticleCate} = require("./artcate_handler");
 
 // 获取文章分类路由
 router.get("/cates", getArticleCates);
+// 新增文章分类
+router.post("/addcates", schema_Handlers(add_ArticleCate), addArticleCate);
 
 
 module.exports = router;
