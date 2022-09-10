49e13929943df61387832dc891207b74a4f47895
XiaoPan <2489964425@qq.com>
2022-09-11 00:16:17 +0800

    定义删除文章分类的路由

--- router/artcate.js
+++ router/artcate.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const {schema_Handlers} = require("../schema/schema_Handlers");
-const {add_ArticleCate} = require("../schema/articles");
-const {getArticleCates, addArticleCate} = require("./artcate_handler");
+const {add_ArticleCate, deleteArticleCate} = require("../schema/articles");
+const {getArticleCates, addArticleCate, deleteArticleCateById} = require("./artcate_handler");
 
 // 获取文章分类路由
 router.get("/cates", getArticleCates);
 // 新增文章分类
 router.post("/addcates", schema_Handlers(add_ArticleCate), addArticleCate);
+// 删除文章分类
+router.get("/deletecate/:id", schema_Handlers(deleteArticleCate), deleteArticleCateById);
 
 
 module.exports = router;
