82fc280398b6c5cea86087c6c372c1f8c46ac666
XiaoPan <2489964425@qq.com>
2022-09-11 21:45:16 +0800

    更新文章分类名称模块的参数验证规则

--- schema/articles.js
+++ schema/articles.js
@@ -19,3 +19,11 @@ exports.getArticleCate = {
         id: Joi.number().integer().min(1).required()
     }
 }
+
+exports.updateArticleCate = {
+    body: {
+        id: Joi.number().integer().min(1).required(),
+        alias: Joi.string().alphanum().required(),
+        title: Joi.string().pattern(/^[\S\u4e00-\u9fa5]{1,4}$/).required()
+    }
+}
