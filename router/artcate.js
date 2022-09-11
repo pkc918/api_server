62c802a4abfd1d48a83b4edf69ec9aa8f7fd65cd
XiaoPan <2489964425@qq.com>
2022-09-11 20:48:06 +0800

    根据 id 获取文章分类路由

--- router/artcate.js
+++ router/artcate.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const {schema_Handlers} = require("../schema/schema_Handlers");
-const {add_ArticleCate, deleteArticleCate} = require("../schema/articles");
-const {getArticleCates, addArticleCate, deleteArticleCateById} = require("./artcate_handler");
+const {add_ArticleCate, deleteArticleCate, getArticleCate} = require("../schema/articles");
+const {getArticleCates, addArticleCate, deleteArticleCateById, getArticleCateById} = require("./artcate_handler");
 
 // 获取文章分类路由
 router.get("/cates", getArticleCates);
@@ -10,6 +10,7 @@ router.get("/cates", getArticleCates);
 router.post("/addcates", schema_Handlers(add_ArticleCate), addArticleCate);
 // 删除文章分类
 router.get("/deletecate/:id", schema_Handlers(deleteArticleCate), deleteArticleCateById);
-
+// 获取文章分类数据
+router.get("/cates/:id", schema_Handlers(getArticleCate), getArticleCateById)
 
 module.exports = router;
