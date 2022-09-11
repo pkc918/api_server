b50bf886dc8b527d9fc951f242a7fe33f6244341
XiaoPan <2489964425@qq.com>
2022-09-12 00:01:54 +0800

    发布新文章 res.body 数据验证规则

---
+++ schema/article.js
@@ -0,0 +1,10 @@
+const Joi = require("joi");
+
+exports.add_article_schema = {
+    body: {
+        title: Joi.string().required(),
+        cate_id: Joi.number().integer().min(1).required(),
+        content: Joi.string().required().allow(""),
+        state: Joi.string().valid("已发布", "草稿").required()
+    }
+};
