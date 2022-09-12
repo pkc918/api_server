cd5756f12372ba3d3a4397a0380f795cca37a7c7
XiaoPan <2489964425@qq.com>
2022-09-12 11:38:07 +0800

    实现 updateArticleById 功能

--- handlers/article_handlers.js
+++ handlers/article_handlers.js
@@ -25,6 +25,30 @@ exports.addArticle = (req, res) => {
     );
 };
 
+// 更新文章
+exports.updateArticle = (req, res) => {
+    // 校验 req.file 数据
+    if (!req.file || req.file.fieldname !== "cover_img") return res.beforeSend(`文章封面未上传!`, 402);
+    const articleInfo = {
+        ...req.body,
+        cover_img: path.join("/uploads", req.file.filename), // 封面存放路径
+        pub_date: new Date(), // 发布时间
+        author_id: req.auth.id // 作者 id
+    };
+    db.query(
+      `update ev_articles set ? where id = ?`,
+      [articleInfo, req.params.id],
+      (err, result) => {
+          if (err) return res.beforeSend(err, 502);
+          if (result.affectedRows !== 1) return res.beforeSend(`更新失败!`, 502);
+          return res.send({
+              status: 200,
+              message: `更新成功!`
+          });
+      }
+    );
+};
+
 // 删除文章
 exports.deleteArticleById = (req, res) => {
     // auth: req.auth.id   id: req.params.id
